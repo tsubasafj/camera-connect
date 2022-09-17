d14a666c4344b83bdd49db5f0db170128b71e093
tsubasafj <105718145+tsubasafj@users.noreply.github.com>
2022-09-17 18:57:20 +0900

    マイページ編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,27 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :camera, :camera_carrer, :model_carrer)
   end
 end
