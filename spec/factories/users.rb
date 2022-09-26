dc6cf6a78543acd9b90788cf1962717ef15e4132
tsubasafj <105718145+tsubasafj@users.noreply.github.com>
2022-09-27 01:06:03 +0900

    Rxpec FactoryBotの導入

---
+++ spec/factories/users.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :user do
+    name { Faker::Name.last_name }
+    email { Faker::Internet.free_email }
+    password { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
+    password_confirmation { password }
+    camera { Faker::Lorem.characters(number: 10) }
+    camera_carrer { Faker::Lorem.characters(number: 10) }
+    model_carrer { Faker::Lorem.characters(number: 10) }
+
+    after(:build) do |user|
+      user.avatar.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+  end
+end
