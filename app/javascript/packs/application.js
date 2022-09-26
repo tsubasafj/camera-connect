7493b50487b779ede60717622d3cd5c33e200cfa
tsubasafj <105718145+tsubasafj@users.noreply.github.com>
2022-09-26 20:18:16 +0900

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,7 @@ require("channels")
 import "bootstrap"
 import "../stylesheets/application"
 require('jquery')
-import "../preview"
+require("../preview")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
