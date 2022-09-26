6b67a849e1614d6760345caf98c0b6907cf09803
tsubasafj <105718145+tsubasafj@users.noreply.github.com>
2022-09-26 21:24:46 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  with_options length: { maximum: 40 } do
+    validates :name, presence: true
+    validates :camera
+    validates :camera_carrer
+    validates :model_carrer
+  end
+
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }, on: :create
+  validates :password, format: { with: VALID_PASSWORD_REGEX }, on: :update, allow_blank: true
 end
