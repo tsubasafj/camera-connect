7122d5ff3fb5c90120a754f05982cc1442333a07
tsubasafj <105718145+tsubasafj@users.noreply.github.com>
2022-09-29 00:19:33 +0900

    バリデーションの設定

--- app/models/board.rb
+++ app/models/board.rb
@@ -4,4 +4,11 @@ class Board < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :want
   belongs_to :prefecture
+
+  with_options presence: true do
+    validates :title
+    validatesn :content
+    validates :want_id, numericality: { other_than: 1 }
+    validates :prefecture_id, numericality: { other_than: 1 }
+  end
 end
