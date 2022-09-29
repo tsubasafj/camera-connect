0271f9522b5278183f5578e3dad24f2c865a0820
tsubasafj <105718145+tsubasafj@users.noreply.github.com>
2022-09-29 21:14:37 +0900

    gemの導入忘れ

--- app/models/board.rb
+++ app/models/board.rb
@@ -7,7 +7,7 @@ class Board < ApplicationRecord
 
   with_options presence: true do
     validates :title
-    validatesn :content
+    validates :content
     validates :want_id, numericality: { other_than: 1 }
     validates :prefecture_id, numericality: { other_than: 1 }
   end
