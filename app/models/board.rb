1880e39b5ab3a4d84e742002e59240c323149ee6
tsubasafj <105718145+tsubasafj@users.noreply.github.com>
2022-09-30 23:50:19 +0900

    Update board.rb

--- app/models/board.rb
+++ app/models/board.rb
@@ -6,8 +6,8 @@ class Board < ApplicationRecord
   belongs_to :prefecture
 
   with_options presence: true do
-    validates :title
-    validates :content
+    validates :title, length: { maximum: 40 }
+    validates :content, length: { maximum: 1000 }
     validates :want_id, numericality: { other_than: 1 }
     validates :prefecture_id, numericality: { other_than: 1 }
   end
