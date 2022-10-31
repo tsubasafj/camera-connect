55c400184f8c934fd4d8bcb2ebf70009203ee17e
tsubasafj <105718145+tsubasafj@users.noreply.github.com>
2022-10-31 23:58:23 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root to: 'boards#index'
   resources :users, only: [:show, :edit, :update]
   resources :boards
+  resources :rooms, only: [:index, :show, :new, :create]
+    
+  end
 end
