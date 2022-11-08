f4b684cc11d5d2f631d40e0c8ebcbf0f945b2edf
tsubasafj <105718145+tsubasafj@users.noreply.github.com>
2022-11-08 13:36:24 +0900

    message投フォームの追加

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -9,7 +9,7 @@ class RoomsController < ApplicationController
     @room = Room.create
     @user_room1 = UserRoom.create(room_id: @room.id, user_id: current_user.id)
     @user_room2 = UserRoom.create(user_room_params)
-    redirect_to "/rooms/#{@room.id}"
+    redirect_to room_path(@room)
   end
 
   def show
@@ -20,10 +20,10 @@ class RoomsController < ApplicationController
       @partner = User.where.not(id:current_user.id)
       @my_messages=Message.where(user_id: current_user.id)
       @other_messages=Message.where(user_id: @partner)
-      @messages=@my_messages.or(@other_messages)#リレーションオブジェクト達を結合する
+      @messages=@my_messages.or(@other_messages)
       @messages=@messages.order(:created_at)
     else
-      redirect_back(fallback_location: root_path)
+      redirect_to user_path(@partner)
     end
   end
 
