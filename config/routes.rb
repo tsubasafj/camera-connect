6c6ef2af787799394b01bd4bb683c3792799c81e
tsubasafj <105718145+tsubasafj@users.noreply.github.com>
2022-12-21 15:02:58 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'boards#index'
   resources :users, only: [:show, :edit, :update]
   resources :boards
-  resources :rooms, only: [:index, :show, :create] do
+  resources :rooms, only: [:index, :show, :create, :destroy] do
     resources :messages, only: :create
   end
 end
